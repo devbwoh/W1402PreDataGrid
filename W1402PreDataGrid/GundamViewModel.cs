9f800183496328f44f1cb68efcfb6708805a9172
BWOH <bwoh@outlook.kr>
2022-12-04 23:28:21 +0900

    초기 커밋.

---
+++ W1402PreDataGrid/GundamViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace W1402PreDataGrid
+{
+    public class Gundam
+    {
+        // prop 탭 2번
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Party { get; set; }
+
+        public Gundam(string name, string model, string party)
+        {
+            Name = name;
+            Model = model;
+            Party = party;
+        }
+    }
+    class GundamViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        ObservableCollection<Gundam> _list = new ObservableCollection<Gundam>();
+        public ObservableCollection<Gundam> List
+        {
+            get { return _list; }
+        }
+
+        public GundamViewModel()
+        {
+
+        }
+
+        public void Add(Gundam gundam)
+        {
+            _list.Add(gundam);
+        }
+    }
+}
---
+++ W1402PreDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace W1402PreDataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GundamViewModel GundamList = new GundamViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Grid.ItemsSource = GundamList.List;
+
+            GundamList.Add(new Gundam("건담", "RX-78-2", "연방군"));
+            GundamList.Add(new Gundam("자쿠II", "MS-06", "지온군"));
+        }
+
+        private void OnAdd(object sender, RoutedEventArgs e)
+        {
+            GundamList.Add(new Gundam("건탱크", "RX-75-4", "연방군"));
+        }
+    }
+}
